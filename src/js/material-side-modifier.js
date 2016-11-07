8de0109078c900a817ea158056df60d69e4baf2c
Joel Gethin Lewis <me@joelgethinlewis.com>
2016-11-07 21:19:00 +0000

    Making material-side-modifier as simple as possible

--- src/js/material-side-modifier.js
+++ src/js/material-side-modifier.js
@@ -1,19 +1,24 @@
 AFRAME.registerComponent('material-side-modifier', {
   // This component can be used multiple times
   multiple: true,
-  // Allow material-side-modifier component default to 2
-  // thanks to @fernandojsg on A-Frame slack for pointing out that the `type` key should be an object:
-  schema: {
-    type: 'int', default: 2
-  },
-  // Update the side property of the material of the component to 2 aka THREE.DoubleSide, see: https://threejs.org/docs/#Reference/Materials/Material
+  // Allow material-side-modifier component a single property schema, of type int, defaulting to 2
+  schema: { default: 2 },
   init: function () {
-  	console.log("A-Frame and the rest of the components have loaded");
-  	var object3D = this.el.object3D;
-  	console.log("Object3D is ", object3D);
+  	console.log("Inside of init() of material-side-modifier");
     var side = this.data; //should be 2, the default value
-    console.log("The type of data (side) is ",this.data.type); //why isn't this reporting properly?
-    console.log("The value of data (side) is ",side); //why isn't this reporting properly?
+    console.log("The value of this.data is ", this.data); //why isn't this reporting properly?
+    console.log("The value of var side is ", side); //why isn't this reporting properly?
+  }
+});
+
+/*
+
+
+    //now object3D logging
+    var object3D = this.el.object3D;
+    console.log("Object3D is ", object3D);
+
+    // Update the side property of the material of the component to 2 aka THREE.DoubleSide, see: https://threejs.org/docs/#Reference/Materials/Material
 
     //this works when typed into console, but not here programmatically, found here: https://threejs.org/docs/#Reference/Materials/Material
     console.log("(Method 1: The current number of sides is", object3D.children[i].material.side);
@@ -30,13 +35,13 @@ AFRAME.registerComponent('material-side-modifier', {
       }
     // via: http://stackoverflow.com/questions/16027131/three-js-how-to-make-double-sided-object
     object3D.traverse( function( node ) {
-    	console.log("Traversing...");
-    	if( node.material ) {
+      console.log("Traversing...");
+      if( node.material ) {
         console.log("Method 3: The current number of sides is",node.material.side);
-    		node.material.side = THREE.DoubleSide;
-    		node.material.needsUpdate = true;
-    		console.log("Method 3: The updated number of sides is",node.material.side);
-    	}
+        node.material.side = THREE.DoubleSide;
+        node.material.needsUpdate = true;
+        console.log("Method 3: The updated number of sides is",node.material.side);
+      }
     })
   }
-});
+*/
