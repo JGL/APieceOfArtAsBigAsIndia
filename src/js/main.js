420d3f2a32b1699d238ebf1c2e0770d726788977
Joel Gethin Lewis <me@joelgethinlewis.com>
2016-11-07 21:23:00 +0000

    Removing kframe as a require in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ var extras = require('aframe-extras');
 // Register everything.
 extras.registerAll();
 require('aframe-mountain-component');
-//require('kframe');
 //requiring my first component! https://github.com/substack/browserify-handbook told me how to do this
 require('./material-side-modifier.js');
 //below are to complete the A-Frame docs component tutorial https://aframe.io/docs/0.3.0/guides/building-with-components.html
