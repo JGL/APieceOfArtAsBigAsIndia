142d236466a8d9b6debb76a9936826a9b5d09d46
Joel Gethin Lewis <me@joelgethinlewis.com>
2016-11-08 21:21:03 +0000

    First attempt at mouse/touch interaction to change mountain Component

--- docs/build.js
+++ docs/build.js
@@ -87536,6 +87536,7 @@ AFRAME.registerComponent('collider-check-and-update', {
     this.el.addEventListener('raycaster-intersected', function () {
       el.components['mountain'].update(); //tip from Don McCurdy
       el.components['mountain'].color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+      //the above doesn't work... why?
     });
   }
 });
