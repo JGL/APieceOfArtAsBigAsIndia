23de7f91545629db951ac4c14ed27a15ca57da64
Joel Gethin Lewis <me@joelgethinlewis.com>
2016-11-08 00:58:49 +0000

    Pushing latest attempt at side modifier component

--- docs/build.js
+++ docs/build.js
@@ -87548,6 +87548,8 @@ require('./set-image.js');
 require('./single-property-schema-bug.js');
 },{"./material-side-modifier.js":55,"./set-image.js":56,"./single-property-schema-bug.js":57,"./update-raycaster.js":58,"aframe":49,"aframe-animation-component":1,"aframe-event-set-component":2,"aframe-extras":3,"aframe-layout-component":33,"aframe-mountain-component":34,"aframe-template-component":46,"aframe-terrain-model-component":47}],55:[function(require,module,exports){
 AFRAME.registerComponent('material-side-modifier', {
+  // This component depends on the material component
+  //dependencies: ['material'],
   // This component can be used multiple times
   multiple: true,
   // Allow material-side-modifier component a single property schema, of type int, defaulting to 2, aka THREE.DoubleSide, see https://threejs.org/docs/#Reference/Materials/Material.side
@@ -87561,29 +87563,21 @@ AFRAME.registerComponent('material-side-modifier', {
   	console.log("Inside of update() of material-side-modifier");
     var side = this.data.side; //should be 2, the default value, all I want to be able to do is material.side = side; - change the side property of the material to
     var object3D = this.el.object3D;
-
-    console.log("Starting traverse of object3D");
-    object3D.traverse( function( child ){
-        console.log("Traversing...")
-        console.log("The current child object is: ", child);
-        console.log("The type of the child is", typeof child);
-        if ( child instanceof THREE.Group ) {
-          console.log("Found a THREE.Group!")
-          child.traverse(function(childOfChild) {
-            console.log("Traversing the traversing...")
-            console.log("The current child of child object is: ", childOfChild);
-            console.log("The type of the child is", typeof childOfChild); //how is the mesh object always one away from me?
-            if ( childOfChild instanceof THREE.Mesh ) {
-              console.log("Found a THREE.Mesh!")
-            }
-          }
-          );
-        }
-      }
-    );
-    console.log("Finished traverse of object3D");
+    var mesh = this.el.getObject3D('mesh');
+    mesh.material.side = THREE.DoubleSide;
   }
 });
+
+    // console.log("Starting traverse of object3D");
+    // object3D.traverse( function( child ){
+    //     console.log("Traversing...")
+    //     console.log("The current child object is: ", child);
+    //     if(child.material){
+    //       child.material.side = THREE.DoubleSide;
+    //     }
+    //   }
+    // );
+    // console.log("Finished traverse of object3D");
 },{}],56:[function(require,module,exports){
 /**
  * Component that listens to an event, fades out an entity, swaps the texture, and fades it
