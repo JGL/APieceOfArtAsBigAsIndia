7be168ee89cbd39a7e091dc5a170affce54a275a
Joel Gethin Lewis <me@joelgethinlewis.com>
2016-11-08 19:46:13 +0000

    Changing the colour of the mountain

--- src/js/collider-check-and-update.js
+++ src/js/collider-check-and-update.js
@@ -3,13 +3,8 @@ AFRAME.registerComponent('collider-check-and-update', {
   init: function () {
     var el = this.el; //tip from Don McCurdy, needed to store the element that this was called from
     this.el.addEventListener('raycaster-intersected', function () {
-      //console.log('Player hit something!');
-      //console.log(this); //where am I?
-      //trying too call the update() method of the Mountain component to which this event listener is now attached
-      //update(); //this errors with Uncaught ReferenceError: update is not defined
-      //this.update(); //this doesn't error, but doesn't do anything
-      //this.el.update(); //this errors with Cannot read property 'update' of undefined
       el.components['mountain'].update(); //tip from Don McCurdy
+      el.components['mountain'].color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
     });
   }
 });
