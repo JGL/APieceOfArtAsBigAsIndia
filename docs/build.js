14b7b9ab370f07f2f6b92c8fb3bbeb9b15afb388
Joel Gethin Lewis <me@joelgethinlewis.com>
2016-11-08 17:21:12 +0000

    Tidying up collider-check-and-update component

--- docs/build.js
+++ docs/build.js
@@ -87532,10 +87532,10 @@ module.exports = (function(){
 AFRAME.registerComponent('collider-check-and-update', {
   dependencies: ['raycaster'],
   init: function () {
-    var el = this.el;
+    var el = this.el; //tip from Don McCurdy, needed to store the element that this was called from
     this.el.addEventListener('raycaster-intersected', function () {
-      console.log('Player hit something!');
-      console.log(this); //where am I?
+      //console.log('Player hit something!');
+      //console.log(this); //where am I?
       //trying too call the update() method of the Mountain component to which this event listener is now attached
       //update(); //this errors with Uncaught ReferenceError: update is not defined
       //this.update(); //this doesn't error, but doesn't do anything
